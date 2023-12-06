b024fc5e1a001825e9f75dda87afa8a005439308
Ckalawa <145289597+Ckalawa@users.noreply.github.com>
2023-12-05 20:40:03 -0600

    Fixes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let currentIndex = 0;
-    const sections = document.querySelectorAll("section[id^='a']");
-  
-    function showSection(index) {
-      sections.forEach((section, i) => {
-        section.style.transform = `translateX(-${index * 100}%)`;
-      });
-    }
-  
-    function nextSection() {
-      currentIndex = (currentIndex + 1) % sections.length;
-      showSection(currentIndex);
-    }
-  
-    function prevSection() {
-      currentIndex = (currentIndex - 1 + sections.length) % sections.length;
-      showSection(currentIndex);
-    }
-  
-    // Attach event listeners to navigation buttons
-    const nextBtn = document.querySelector(".swiper-button-next");
-    const prevBtn = document.querySelector(".swiper-button-prev");
-  
-    nextBtn.addEventListener("click", nextSection);
-    prevBtn.addEventListener("click", prevSection);
-  });
-  
-  
+const images = ["A220.png", "A300.png", "A310.png", "A321.png", "A340.png", "A350.png", "A380.png"];
+let currentIndex = 0;
+
+function updateCarousel() {
+    const carousel = document.getElementById('carousel');
+    carousel.style.backgroundImage = `url(${images[currentIndex]})`;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+}
+
+function initCarousel() {
+    const carouselContainer = document.createElement('div');
+    carouselContainer.id = 'carousel';
+    document.body.appendChild(carouselContainer);
+
+    const prevButton = document.createElement('button');
+    prevButton.textContent = 'Previous';
+    prevButton.addEventListener('click', prevSlide);
+    document.body.appendChild(prevButton);
+
+    const nextButton = document.createElement('button');
+    nextButton.textContent = 'Next';
+    nextButton.addEventListener('click', nextSlide);
+    document.body.appendChild(nextButton);
+
+    updateCarousel();
+}
+
+window.addEventListener('load', initCarousel);
