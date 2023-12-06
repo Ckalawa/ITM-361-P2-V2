17e7f3cce592ad9b6a5ba2fdd69e71097027f3f5
Ckalawa <145289597+Ckalawa@users.noreply.github.com>
2023-12-05 21:02:19 -0600

    fixes

--- js/script.js
+++ js/script.js
@@ -1,37 +1,30 @@
-const images = ["A220.png", "A300.png", "A310.png", "A321.png", "A340.png", "A350.png", "A380.png"];
-let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function () {
+  var slider = document.getElementById('slider');
+  var images = slider.getElementsByTagName('img');
+  var currentIndex = 0;
 
-function updateCarousel() {
-    const carousel = document.getElementById('carousel');
-    carousel.style.backgroundImage = `url(${images[currentIndex]})`;
-}
+  function showImage(index) {
+      for (var i = 0; i < images.length; i++) {
+          images[i].style.display = 'none';
+      }
+      images[index].style.display = 'block';
+  }
 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateCarousel();
-}
+  function nextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      showImage(currentIndex);
+  }
 
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateCarousel();
-}
+  function prevImage() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      showImage(currentIndex);
+  }
 
-function initCarousel() {
-    const carouselContainer = document.createElement('div');
-    carouselContainer.id = 'carousel';
-    document.body.appendChild(carouselContainer);
+  showImage(currentIndex);
 
-    const prevButton = document.createElement('button');
-    prevButton.textContent = 'Previous';
-    prevButton.addEventListener('click', prevSlide);
-    document.body.appendChild(prevButton);
-
-    const nextButton = document.createElement('button');
-    nextButton.textContent = 'Next';
-    nextButton.addEventListener('click', nextSlide);
-    document.body.appendChild(nextButton);
-
-    updateCarousel();
-}
-
-window.addEventListener('load', initCarousel);
+  slider.addEventListener('click', nextImage);
+  slider.addEventListener('contextmenu', function (event) {
+      event.preventDefault();
+      prevImage();
+  });
+});
